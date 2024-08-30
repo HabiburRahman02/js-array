6c221d1d339af6a97aabaed69f07785748ce4b4b
Habibur Rahman <habibur88454@gmail.com>
2024-08-30 23:38:45 +0600

    added push and pop

---
+++ push.pop.js
@@ -0,0 +1,15 @@
+const numbers = [23, 56, 67, 34]
+console.log(numbers);
+
+numbers.push(111)
+console.log(numbers)
+
+numbers.push(55)
+console.log(numbers)
+
+// pop
+const ages = [10, 20, 40, 30]
+console.log(ages)
+const out1 = ages.pop();
+console.log(out1);
+console.log(ages)
