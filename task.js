3107598027f6f238832cd24c7e3f217aaf47310b
Habibur Rahman <habibur88454@gmail.com>
2024-08-31 01:15:57 +0600

    solved some js  task

---
+++ task.js
@@ -0,0 +1,52 @@
+// task-01
+// const fruits = ['banana', 'guaba', 'apple', 'orange'];
+// console.log(fruits)
+// const thirdIndex = fruits[3];
+// fruits[2] = 'notkor'
+
+// console.log(thirdIndex)
+// console.log(fruits)
+
+
+// task-02
+// const spots = ['kuakata', 'sajek', 'modhutila'];
+// console.log(spots);
+// spots.push('gojni')
+// spots.push('sitakunjo')
+// console.log(spots);
+// spots.pop();
+// console.log(spots)
+
+
+// task-03
+// const bookNames = ['python', 'js', 'php'];
+// console.log(bookNames.includes('php')); 
+
+// if(bookNames.includes('js')){
+//     console.log('array te js book ache')
+// }
+// else{
+//     console.log('js book nai')
+// }
+
+
+// task-04
+// const friends = ['abul', 'cabul', 'kabul'];
+// const name = 'josim'
+
+// console.log(Array.isArray(friends))
+// console.log(Array.isArray(name))
+
+// if(Array.isArray(friends)){
+//     console.log(' Its an array')
+// }
+// else{
+//     console.log('Its not an array')
+// }
+
+
+// task-05
+const firstNumbers = [12, 34, 56];
+const secondNumbers = [67, 45, 34];
+const totalNumbers  = firstNumbers.concat(secondNumbers)
+console.log(totalNumbers)
