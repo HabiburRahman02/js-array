7db265283c21e8ae7c2ef6198a706708d6487dfb
Habibur Rahman <habibur88454@gmail.com>
2024-08-31 00:18:40 +0600

    apply join and isArray

---
+++ isArray.js
@@ -0,0 +1,7 @@
+const names = ['jamal', 'kamal', 'rahim', 'karim'];
+const numb = [];
+const age = 34
+
+console.log(Array.isArray(names))
+console.log(Array.isArray(numb))
+console.log(Array.isArray(age))
---
+++ join.js
@@ -0,0 +1,4 @@
+const names = ['jamal', 'kamal', 'rahim', 'karim'];
+console.log(names)
+// console.log(names.join(' '))
+console.log(names.join('_'))
