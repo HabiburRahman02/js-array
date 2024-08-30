c331838272965cab4414f0bb799f86f295ddb325
Habibur Rahman <habibur88454@gmail.com>
2024-08-31 00:08:53 +0600

    apply includes method

---
+++ includes.js
@@ -0,0 +1,11 @@
+const names = ['jamal', 'kamal', 'rahim', 'karim'];
+
+console.log(names.includes('jamal'));
+console.log(names.includes('ferdous'));
+
+if(names.includes('rahim')){
+    console.log('tor nam ase mama')
+}
+else{
+    console.log('tor nam nai ja vag')
+}
