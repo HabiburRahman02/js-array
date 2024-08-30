65ace54a1d26d13323bd4e15a0ac554809c41f27
Habibur Rahman <habibur88454@gmail.com>
2024-08-31 00:23:12 +0600

    apply concat method

---
+++ concat.js
@@ -0,0 +1,9 @@
+const oldAge = [12, 32, 44];
+const newAge = [99, 66, 77];
+const array3 = [90, 50, 40]
+
+const newAgeConcat = oldAge.concat(newAge,array3) /// joto value add korbo koma diye bosiye dibo
+console.log(newAgeConcat)
+
+console.log(oldAge);
+console.log(newAge);
