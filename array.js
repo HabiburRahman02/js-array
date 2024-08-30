47e65f9870ba9ce90cf19e1d7043f78e45c0e516
Habibur Rahman <habibur88454@gmail.com>
2024-08-30 21:06:17 +0600

    apply array and length method

---
+++ array.js
@@ -0,0 +1,3 @@
+const numbers = [12, 34, 56, 78, 903, 34, 32];
+const friends = ['amir', 'jamal', 'akbor', 'nasim'];
+console.log(friends)
---
+++ length.index.js
@@ -0,0 +1,12 @@
+const numbers = [12, 34, 56, 78, 34, 32];
+console.log(numbers)
+console.log(numbers[3])
+
+const first = numbers[0];
+// console.log(first)
+
+console.log(numbers.indexOf(78))
+
+
+numbers[4] = 9345;
+console.log(numbers)
